cf3252a129310743c3caf203f086189f986def5a
arjun dev pk <cb.en.u4cse18208@cb.students.amrita.edu>
2020-04-21 16:26:03 +0530

    gitignore added for  node

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const volleyball = require('volleyball');
 const cors = require('cors');
+const path = require('path');
 const auth = require('./auth/auth.routes');
 
 
@@ -17,10 +18,22 @@ app.use(volleyball);
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+
+app.use(express.static(path.join(__dirname, '../public/')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
 app.use(middlewares.checkTokenSetUser);
 
+// Handle production
 
-app.get('/', (req, res) => {
+if (process.env.NODE_ENV === 'production') {
+  // static folder
+  app.use(express.static(path.join(__dirname, '../public/')));
+  app.get('/.*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+}
+
+
+app.get('/me', (req, res) => {
   res.json({
     message: 'Hello World!😍',
     user: req.user,
@@ -47,5 +60,6 @@ function errorHandler(err, req, res, next) {
 
 app.use(notFound);
 app.use(errorHandler);
+
 
 module.exports = app;
